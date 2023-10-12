38f3ffdbd1b60bba170ec262bcb53c9b81d9cf8c
Astuti08 <astutisolihatunnisa1502@gmail.com>
2023-10-12 16:47:09 +0700

    add index.html

---
+++ main.js
@@ -0,0 +1,36 @@
+console.log("Hello World")
+alert  ("Ada Notifikasi")
+prompt ("Pinjam Seratus")
+
+var Promnet="Coding is easy"
+console.log(Promnet)
+var Promnet="Coding is easy"
+console.log(Promnet)
+var Promnet="nothing is possible"
+console.log (Promnet)
+
+let Promnet="Coding is fun"
+console.log(Promnet)
+let Promnet="Coding not fun"
+console.log(Promnet)
+
+const Promnet="Coding is my life"
+console.log(Promnet)
+const Promnet="Coding is not my life"
+console.log(Promnet)
+
+let totalPoin=prompt("Masukkan poin anda")
+    if(totalPoin>=100){
+        document.write("congratulation")
+    }else{
+        document.write("Keep Triying")
+    }
+
+let x=6;
+let y=3;
+
+console.log(x<10&&y>1)
+console.log(x<10&&y<1)
+console.log(x==5||y==5)
+console.log(x==6||y==5)
+!(x==y)
